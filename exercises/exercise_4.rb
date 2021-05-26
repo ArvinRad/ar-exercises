7f5c5f11972dea53900730742678880885bf03c4
ArvinRad <radanarvin@gmail.com>
2021-05-26 07:38:55 +0000

    4th exercises

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,30 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel:false,
+  womens_apparel:true
+)
+Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel:true,
+  womens_apparel: false
+)
+Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel:true,
+  womens_apparel:true
+)
+@mens_stores = Store.having(mens_apparel = true && womens_apparel = false)
+@mens_stores.each do |t|
+  puts "Store Name: #{t.name}, Annual Revenue: #{t.annual_revenue}"
+end
+@womens_stores = Store.having(mens_apparel = false && womens_apparel == true && annual_revenue < 1000000)
+@womens_stores.each do |t|
+  puts "Store Name: #{t.name}, Annual Revenue: #{t.annual_revenue}"
+end
+
